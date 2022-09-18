703dde3352b214ef6eb3dc554d605671e830d7c1
qmacias <ezequielqmacias@gmail.com>
2022-09-18 08:45:56 -0300

    v1.3 add dynamic web content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
+
+import { JsonService, Portfolio } from './services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portfolio-front-end';
+export class AppComponent implements OnInit {
+  dateString: string;
+  jsonData!: Portfolio;
+
+  constructor(
+    @Inject(LOCALE_ID) private locale: string,
+    private jsonService: JsonService
+  ) {
+    this.dateString = formatDate(Date.now(), 'yyyy', this.locale);
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  private loadData(): void {
+    this.jsonService.getAll().subscribe({
+      next: (data: Portfolio) => { this.jsonData = data },
+      error: (e) => console.log(e)
+    });
+  }
 }
---
+++ src/app/services/json/json.service.ts
@@ -0,0 +1,142 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JsonService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Portfolio> {
+    return this.http.get<Portfolio>('assets/data/portfolio.json');
+  }
+}
+
+export class Portfolio {
+  user: User;
+  constructor(obj?: any) {
+    this.user = obj?.user || null;
+  }
+}
+
+export class User {
+  id: string;
+  name: string;
+  age: number;
+  degree: string;
+  email: string;
+  phone: string;
+  summary: string;
+  image: Image;
+  social: Social;
+  skills: Skill[];
+  jobs: Job[];
+  projects: Project[];
+  constructor(obj?: any) {
+    this.id = obj?.id || null;
+    this.name = obj?.name || null;
+    this.age = obj?.age || null;
+    this.degree = obj?.degree || null;
+    this.email = obj?.email || null;
+    this.phone = obj?.phone || null;
+    this.summary = obj?.summary || null;
+    this.image = obj?.image || null;
+    this.social = obj?.social || null;
+    this.skills = obj?.skills || null;
+    this.jobs = obj?.jobs || null;
+    this.projects = obj?.projects || null;
+  }
+}
+
+export class Image {
+  id: string;
+  path: string;
+  constructor(obj?: any) {
+    this.id = obj?.id || null;
+    this.path = obj?.path || null;
+  }
+}
+
+export class Social {
+  id: string;
+  link: string;
+  constructor(obj?: any) {
+    this.id = obj?.id || null;
+    this.link = obj?.link || null;
+  }
+}
+
+export class Skill {
+  id: string;
+  name: string;
+  tools: Tool[];
+  constructor(obj?: any) {
+    this.id = obj?.id || null;
+    this.name = obj?.name || null;
+    this.tools = obj?.tools || null;
+  }
+}
+
+export class Tool {
+  id: string;
+  name: string;
+  constructor(obj?: any) {
+    this.id = obj?.id || null;
+    this.name = obj?.name || null;
+  }
+}
+
+export class Job {
+  id: string;
+  position: string;
+  description: string;
+  startDate: string;
+  finishDate: string;
+  achievements: Achievement[];
+  constructor(obj?: any) {
+    this.id = obj?.id || null;
+    this.position = obj?.position || null;
+    this.description = obj?.description || null;
+    this.startDate = obj?.startDate || null;
+    this.finishDate = obj?.finishDate || null;
+    this.achievements = obj?.achievements || null;
+  }
+}
+
+export class Achievement {
+  id: string;
+  description: string;
+  constructor(obj?: any) {
+    this.id = obj?.id || null;
+    this.description = obj?.description || null;
+  }
+}
+
+export class Project {
+  id: string;
+  name: string;
+  description: string;
+  releaseDate: string;
+  image: Image;
+  technologies: Technology[];
+  constructor(obj?: any) {
+    this.id = obj?.id || null;
+    this.name = obj?.name || null;
+    this.description = obj?.description || null;
+    this.releaseDate = obj?.releaseDate || null;
+    this.image = obj?.image || null;
+    this.technologies = obj?.technologies || null;
+  }
+}
+
+export class Technology {
+  id: string;
+  name: string;
+  constructor(obj?: any) {
+    this.id = obj?.id || null;
+    this.name = obj?.name || null;
+  }
+}
